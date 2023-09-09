695f52eab2c321229cb86bd3fc864df0b78f4364
David Jin <kimdaxter@gmail.com>
2023-09-09 15:31:06 -0700

    add new component, new page and data resources

---
+++ src/components/Card.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import * as images from "../image";
+	interface Quiz {
+		id: number;
+		name: string;
+		imageUrl: "chemistry" | "biology";
+		questions: {
+			id: number;
+			text: string;
+			answer: string;
+			options: {
+				id: number;
+				text: string;
+				label: string;
+			}[];
+		}[];
+	}
+	export let quiz: Quiz;
+</script>
+
+<div class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer m-2">
+	<img class="h-44" src={images[quiz.imageUrl]} alt="" />
+	<div class="flex items-center justify-between py-3 px-2">
+		<h1 class="font-bold">{quiz.name}</h1>
+		<p>{quiz.questions.length} questions</p>
+	</div>
+</div>
---
+++ src/routes/quiz/[id]/+page.svelte
@@ -0,0 +1,3 @@
+<div>
+	<h1>Quiz Page</h1>
+</div>
