b0d51796ca8e462ed8ebcb3a3bbdaa76313d25d3
David Jin <kimdaxter@gmail.com>
2023-09-13 21:52:02 -0700

    add global store; update button effect; pass page function to component; update click logic

--- src/routes/quiz/[id]/+page.svelte
+++ src/routes/quiz/[id]/+page.svelte
@@ -2,18 +2,57 @@
 	import QuestionButton from "./components/QuestionButton.svelte";
 	import QuestionOption from "./components/QuestionOption.svelte";
 	import QuestionText from "./components/QuestionText.svelte";
+	import { answers, type Answer } from "../../../store";
+	import { goto } from "$app/navigation";
 
 	export let data: any;
 	let currentQuestionIndex = 0;
+	let answersValue: Answer[];
+	let selectedOption: null | string = null;
+	let showCorrectAnswer = false;
+
+	const handleChangeOption = (label: string) => {
+		selectedOption = label;
+	};
+
+	const handleNext = () => {
+		showCorrectAnswer = false;
+		selectedOption = null;
+		if (currentQuestionIndex === data.questions.length - 1) {
+			goto("/results");
+		} else {
+			currentQuestionIndex += 1;
+		}
+	};
+	const handleSubmit = () => {
+		if (!selectedOption) return;
+		showCorrectAnswer = true;
+		answers.update((currentState) => {
+			const updatedAnswerState = currentState;
+			updatedAnswerState[currentQuestionIndex].isCorrect = selectedOption === question.answer;
+			return updatedAnswerState;
+		});
+	};
+	answers.subscribe((value) => {
+		answersValue = value;
+	});
+
 	$: question = data.questions[currentQuestionIndex];
 </script>
 
 <div class="w-full">
 	<QuestionText text={question.question} />
+
 	<div class="flex justify-between flex-wrap cursor-pointer">
 		{#each question.options as option (option.id)}
-			<QuestionOption {option} />
+			<QuestionOption
+				{option}
+				{selectedOption}
+				{handleChangeOption}
+				{showCorrectAnswer}
+				answer={question.answer}
+			/>
 		{/each}
 	</div>
-	<QuestionButton />
+	<QuestionButton {handleSubmit} {showCorrectAnswer} {handleNext} />
 </div>
